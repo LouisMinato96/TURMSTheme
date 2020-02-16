174954f37babbf71696ff58145981449a1fe9ed5
shubham.m@bigphi.com <shubham.m@bigphi.com>
2020-02-16 11:59:51 +0530

    cube

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularHomeComponent } from './components/angular-home/angular-home.component';
+import { CubeComponent } from './components/cube/cube.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AngularHomeComponent
+    AngularHomeComponent,
+    CubeComponent
   ],
   imports: [
     BrowserModule,
