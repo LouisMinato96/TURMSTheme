b482b286befe1b298bc3d1c5f446e7ef64c1d53f
shubham.m@bigphi.com <shubham.m@bigphi.com>
2020-02-16 12:01:02 +0530

    cube

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AngularHomeComponent } from './components/angular-home/angular-home.component';
+import { CubeComponent } from './components/cube/cube.component';
 
 
 const routes: Routes = [
   { path: 'angularHome', component: AngularHomeComponent },
-  { path: '**', redirectTo: '/angularHome', pathMatch: 'full'  }
+  { path: 'cube', component: CubeComponent },
+  { path: '**', redirectTo: '/cube', pathMatch: 'full'  }
 ];
 
 @NgModule({
