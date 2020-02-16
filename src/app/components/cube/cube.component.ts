7c93b70550795b7d7b36fc3386fb07d346c0abcf
shubham.m@bigphi.com <shubham.m@bigphi.com>
2020-02-16 17:34:54 +0530

    box design done

--- src/app/components/cube/cube.component.ts
+++ src/app/components/cube/cube.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CubeComponent implements OnInit {
 
+  blockUnit = 300;
+  initiated: boolean = false;
+  unitGlowClass: string = 'unit-Glow-1';
+
   constructor() { }
 
+  initiateBoxMaking(){
+    setTimeout(() => {
+      this.initiated = true;
+    },200);
+  }
+
   ngOnInit() {
+    this.initiateBoxMaking();
   }
 
 }
