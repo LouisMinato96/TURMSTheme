dd1c93c50ac531ab3d788e0d8b88ef69c2db66a7
shubham.m@bigphi.com <shubham.m@bigphi.com>
2020-02-17 08:24:30 +0530

    cube done

--- src/app/components/cube/cube.component.ts
+++ src/app/components/cube/cube.component.ts
@@ -10,17 +10,34 @@ export class CubeComponent implements OnInit {
   blockUnit = 300;
   initiated: boolean = false;
   unitGlowClass: string = 'unit-Glow-1';
+  face1_AnimationDuration = '9000ms';
+  face2_AnimationDuration = '9000ms';
+  face3_AnimationDuration = '9000ms';
+  face4_AnimationDuration = '9000ms';
+  face5_AnimationDuration = '9000ms';
+  face6_AnimationDuration = '9000ms';
 
   constructor() { }
+
+  ngOnInit() {
+    this.initiateBoxMaking();
+  }
 
   initiateBoxMaking(){
     setTimeout(() => {
       this.initiated = true;
     },200);
+    this.unitGlowClass = 'unit-Glow-' + this.getRandomInt( 1, 5);
+    this.face1_AnimationDuration = this.getRandomInt(8000, 10000) + 'ms';
+    this.face2_AnimationDuration = this.getRandomInt(8000, 10000) + 'ms';
+    this.face3_AnimationDuration = this.getRandomInt(8000, 10000) + 'ms';
+    this.face4_AnimationDuration = this.getRandomInt(8000, 10000) + 'ms';
+    this.face5_AnimationDuration = this.getRandomInt(8000, 10000) + 'ms';
+    this.face6_AnimationDuration = this.getRandomInt(8000, 10000) + 'ms';
   }
 
-  ngOnInit() {
-    this.initiateBoxMaking();
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
   }
 
 }
