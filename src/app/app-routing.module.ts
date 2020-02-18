771b54081a05afe190fd937e2ecdb9ccc8f9a8c9
shubham.m@bigphi.com <shubham.m@bigphi.com>
2020-02-18 08:07:38 +0530

    Array made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AngularHomeComponent } from './components/angular-home/angular-home.component';
 import { CubeComponent } from './components/cube/cube.component';
+import { CustomGalaxyComponent } from './components/custom-galaxy/custom-galaxy.component';
 
 
 const routes: Routes = [
   { path: 'angularHome', component: AngularHomeComponent },
   { path: 'cube', component: CubeComponent },
-  { path: '**', redirectTo: '/cube', pathMatch: 'full'  }
+  { path: 'galaxy', component: CustomGalaxyComponent },
+  { path: '**', redirectTo: '/galaxy', pathMatch: 'full'  }
 ];
 
 @NgModule({
